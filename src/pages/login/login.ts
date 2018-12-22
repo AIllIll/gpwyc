445eb0c5aa21a695c77cbd4d2687ac3b0603e0b2
AIllIll <864147974@qq.com>
2018-12-22 16:05:12 +0800

    add component userInfo, footer

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -5,10 +5,31 @@ import {pagify, MyPage, wxp} from 'base/'
 @pagify()
 export default class extends MyPage {
   data = {
-
+    
   }
 
   async onLoad(options: any) {
     console.log(await wxp.getUserInfo())
+  }
+
+  getUserInfo(e:any) {
+    console.log(e)
+    this.store.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo
+    })
+  }
+
+  wechatLogin(){
+    if(this.store.userInfo){
+      wxp.switchTab({
+        url:"../conversations/conversations"
+      })
+    }else{
+      wxp.showToast({
+        title:"请先获取微信授权",
+        icon:"none"
+      })
+    }
   }
 }
